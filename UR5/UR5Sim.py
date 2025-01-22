aa45d491894a868a6b9c5810fb696d9e05475d52
Curry50 <872740080@qq.com>
2025-01-22 15:45:08 +0800

    first commit

--- UR5/UR5Sim.py
+++ UR5/UR5Sim.py
@@ -1,138 +1,88 @@
 import os
-import math 
 import numpy as np
-import time
-import pybullet 
-import random
 from datetime import datetime
-import pybullet_data
 from collections import namedtuple
 from attrdict import AttrDict
+from IK import UR5sim
+import time
+import math
+from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import PPO
+from rl_env import MagneticReach
+import argparse
 
-ROBOT_URDF_PATH = "./ur_e_description/urdf/ur5e.urdf"
-TABLE_URDF_PATH = os.path.join(pybullet_data.getDataPath(), "table/table.urdf")
+class choose_simulation_type():
+    def __init__(self):
+        pass
 
-class UR5Sim():
-  
-    def __init__(self, camera_attached=False):
-        pybullet.connect(pybullet.GUI)
-        pybullet.setRealTimeSimulation(True)
-        
-        self.end_effector_index = 7
-        self.ur5 = self.load_robot()
-        self.num_joints = pybullet.getNumJoints(self.ur5)
-        
-        self.control_joints = ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
-        self.joint_type_list = ["REVOLUTE", "PRISMATIC", "SPHERICAL", "PLANAR", "FIXED"]
-        self.joint_info = namedtuple("jointInfo", ["id", "name", "type", "lowerLimit", "upperLimit", "maxForce", "maxVelocity", "controllable"])
-
-        self.joints = AttrDict()
-        for i in range(self.num_joints):
-            info = pybullet.getJointInfo(self.ur5, i)
-            jointID = info[0]
-            jointName = info[1].decode("utf-8")
-            jointType = self.joint_type_list[info[2]]
-            jointLowerLimit = info[8]
-            jointUpperLimit = info[9]
-            jointMaxForce = info[10]
-            jointMaxVelocity = info[11]
-            controllable = True if jointName in self.control_joints else False
-            info = self.joint_info(jointID, jointName, jointType, jointLowerLimit, jointUpperLimit, jointMaxForce, jointMaxVelocity, controllable)
-            if info.type == "REVOLUTE":
-                pybullet.setJointMotorControl2(self.ur5, info.id, pybullet.VELOCITY_CONTROL, targetVelocity=0, force=0)
-            self.joints[info.name] = info     
-
-
-    def load_robot(self):
-        flags = pybullet.URDF_USE_SELF_COLLISION
-        table = pybullet.loadURDF(TABLE_URDF_PATH, [0.5, 0, -0.6300], [0, 0, 0, 1])
-        robot = pybullet.loadURDF(ROBOT_URDF_PATH, [0, 0, 0], [0, 0, 0, 1], flags=flags)
-        return robot
+    def demo_slider_simulation(self):
+        """ Demo program showing how to use the sim """
+        env = UR5sim()
+        env.add_gui_sliders()
+        while True:
+            x, y, z, Rx, Ry, Rz = env.read_gui_sliders()
+            joint_angles = env.calculate_ik([x, y, z], [Rx, Ry, Rz])
+            # print(joint_angles)
+            env.draw_debug_line(x,y,z)
+            env.set_joint_angles(joint_angles)
+            env.check_collisions()
     
-
-    def set_joint_angles(self, joint_angles):
-        poses = []
-        indexes = []
-        forces = []
-
-        for i, name in enumerate(self.control_joints):
-            joint = self.joints[name]
-            poses.append(joint_angles[i])
-            indexes.append(joint.id)
-            forces.append(joint.maxForce)
-
-        pybullet.setJointMotorControlArray(
-            self.ur5, indexes,
-            pybullet.POSITION_CONTROL,
-            targetPositions=joint_angles,
-            targetVelocities=[0]*len(poses),
-            positionGains=[0.04]*len(poses), forces=forces
-        )
-
-
-    def get_joint_angles(self):
-        j = pybullet.getJointStates(self.ur5, [1,2,3,4,5,6])
-        joints = [i[0] for i in j]
-        return joints
-    
-
-    def check_collisions(self):
-        collisions = pybullet.getContactPoints()
-        if len(collisions) > 0:
-            print("[Collision detected!] {}".format(datetime.now()))
-            return True
-        return False
-
-
-    def calculate_ik(self, position, orientation):
-        quaternion = pybullet.getQuaternionFromEuler(orientation)
-        lower_limits = [-math.pi]*6
-        upper_limits = [math.pi]*6
-        joint_ranges = [2*math.pi]*6
-        rest_poses = [0, -math.pi/2, -math.pi/2, -math.pi/2, -math.pi/2, 0]
-
-        joint_angles = pybullet.calculateInverseKinematics(
-            self.ur5, self.end_effector_index, position, quaternion, 
-            jointDamping=[0.01]*6, upperLimits=upper_limits, 
-            lowerLimits=lower_limits, jointRanges=joint_ranges, 
-            restPoses=rest_poses
-        )
-        return joint_angles
-       
-
-    def add_gui_sliders(self):
-        self.sliders = []
-        self.sliders.append(pybullet.addUserDebugParameter("X", 0, 1, 0.4))
-        self.sliders.append(pybullet.addUserDebugParameter("Y", -1, 1, 0))
-        self.sliders.append(pybullet.addUserDebugParameter("Z", 0.3, 1, 0.4))
-        self.sliders.append(pybullet.addUserDebugParameter("Rx", -math.pi/2, math.pi/2, 0))
-        self.sliders.append(pybullet.addUserDebugParameter("Ry", -math.pi/2, math.pi/2, 0))
-        self.sliders.append(pybullet.addUserDebugParameter("Rz", -math.pi/2, math.pi/2, 0))
-
-
-    def read_gui_sliders(self):
-        x = pybullet.readUserDebugParameter(self.sliders[0])
-        y = pybullet.readUserDebugParameter(self.sliders[1])
-        z = pybullet.readUserDebugParameter(self.sliders[2])
-        Rx = pybullet.readUserDebugParameter(self.sliders[3])
-        Ry = pybullet.readUserDebugParameter(self.sliders[4])
-        Rz = pybullet.readUserDebugParameter(self.sliders[5])
-        return [x, y, z, Rx, Ry, Rz]
+    def demo_circle_simulation(self):
+        env = UR5sim()
+        time = 0
+        [Rx, Ry, Rz] = [math.pi/2,math.pi/2,-math.pi/2]
+        while True:
+            time += env.time_step
+            [x,y,z] = [0.2 + 0.05*math.sin(time),0.0+0.05*math.cos(time),0.5]
+            joint_angles = env.calculate_ik([x, y, z], [Rx, Ry, Rz])
         
-    def get_current_pose(self):
-        linkstate = pybullet.getLinkState(self.ur5, self.end_effector_index, computeForwardKinematics=True)
-        position, orientation = linkstate[0], linkstate[1]
-        return (position, orientation)
+            env.draw_debug_line(x,y,z)
 
-def demo_simulation():
-    """ Demo program showing how to use the sim """
-    sim = UR5Sim()
-    sim.add_gui_sliders()
-    while True:
-        x, y, z, Rx, Ry, Rz = sim.read_gui_sliders()
-        joint_angles = sim.calculate_ik([x, y, z], [Rx, Ry, Rz])
-        sim.set_joint_angles(joint_angles)
-        sim.check_collisions()
+            env.set_joint_angles(joint_angles)
+            env.check_collisions()
+        
+    def demo_joint_space_rl(self,test=False,visual=False):
+        model_path = 'magnetic_reach.zip'
+
+        robot = UR5sim(vis=visual)
+        env = MagneticReach(robot,camera=None,vis=visual)
+        if test == True:
+            model = PPO.load(model_path)
+            obs,_ = env.reset()
+            done = False
+            while not done:
+                 action,states = model.predict(obs)
+                 obs, reward, done, _, info = env.step(action)
+                 print('distance:',env.distance_3d)
+        else:
+            env.reset()
+            if os.path.exists(model_path):
+                print('continue!')
+                model = PPO.load(model_path, env=env,device='cpu')
+                model.learn(total_timesteps=250000)
+
+            else:
+                print('start!')
+                model = PPO("MlpPolicy", env, verbose=1,device='cpu')
+                model.learn(total_timesteps=250000)
+                
+            
+            model.save(model_path)
+    
+def parse_arguments():
+    parse = argparse.ArgumentParser(description="parse arguments")
+    parse.add_argument('--test',action="store_true",help='test')
+    # parse.add_argument('--train',action="store_false",help='train')
+    parse.add_argument('--render',action="store_true",help='render')
+    # parse.add_argument('--no_render',action="store_false",help='do not render')
+    args = parse.parse_args()
+    return args
+
+        
 
 if __name__ == "__main__":
-    demo_simulation()
+    args = parse_arguments()
+    sim = choose_simulation_type()
+    # sim.demo_circle_simulation()
+    # sim.demo_slider_simulation()
+    sim.demo_joint_space_rl(args.test,args.render)
---
+++ UR5/rl_env.py
@@ -0,0 +1,139 @@
+import time
+import math
+import random
+
+import numpy as np
+import pybullet as p
+import pybullet_data
+
+from collections import namedtuple
+from attrdict import AttrDict
+from tqdm import tqdm
+import gymnasium as gym
+from gymnasium import spaces
+
+class FailToReachTargetError(RuntimeError):
+    pass
+
+
+class MagneticReach(gym.Env):
+
+    SIMULATION_STEP_DELAY = 1 / 20.
+
+    def __init__(self,robot,camera=None,vis=False) -> None:
+        self.robot = robot
+        self.vis = vis
+        self.camera = camera
+        self.step_counter = 0
+
+        self.height = 0.25
+        self.x_lower_bound = 0.1
+        self.x_higher_bound = 0.25
+        self.y_lower_bound = 0.4
+        self.y_higher_bound = 0.7
+        self.z_lower_bound = 0.15
+        self.z_higher_bound = 0.35
+
+
+        # define environment
+        # self.physicsClient = p.connect(p.GUI if self.vis else p.DIRECT)
+        # self.planeID = p.loadURDF("plane.urdf")
+
+        # load UR5 and gripper
+        # ur5_id = self.robot.load_robot()
+
+        self.low_obs = np.array([-0.5 for i in range(6)])
+        self.high_obs = np.array([0.5 for i in range(6)])
+        self.action_space = spaces.Box(low=-1,high=1,shape=(6,))
+        self.observation_space = spaces.Box(low=self.low_obs,high=self.high_obs)
+        
+        # target position
+        x = np.random.uniform(self.x_lower_bound, self.x_higher_bound)
+        y = np.random.uniform(self.y_lower_bound, self.y_higher_bound)
+        z = self.height
+        self.target_position = np.array([x,y,z])
+        self.line_id = p.addUserDebugLine([x,y,z],[x,y,z+0.2],[1,0,0],4)
+        (self.ee_pos,self.ee_ori) = self.robot.get_current_pose() 
+        self.distance_3d = np.array(self.ee_pos)-self.target_position
+
+    def step_simulation(self):
+        """
+        Hook p.stepSimulation()
+        """
+        p.stepSimulation()
+        if self.vis:
+            time.sleep(self.SIMULATION_STEP_DELAY)
+
+    def step(self, action, control_method='joint'):
+        """
+        action: (x, y, z, roll, pitch, yaw, gripper_opening_length) for End Effector Position Control
+                (a1, a2, a3, a4, a5, a6, a7, gripper_opening_length) for Joint Position Control
+        control_method:  'end' for end effector position control
+                         'joint' for joint position control
+        """
+        assert control_method in ('joint', 'end')
+        action = [action[i] * math.pi for i in range(6)]
+        self.robot.set_joint_angles(action)
+        # self.robot.move_gripper(action[-1])
+
+        # self.joint_velocity_pre = self.robot.get_joint_obs()['velocities']
+
+        # for _ in range(5):  # Wait for a few steps
+        self.step_simulation()
+
+        self.step_counter += 1
+        # self.obs = self.robot.get_joint_obs()
+        (self.ee_pos,self.ee_ori) = self.robot.get_current_pose() 
+        self.distance_3d = np.array(self.ee_pos)-self.target_position
+        # self.joint_velocity = self.robot.get_joint_obs()['velocities']
+        # self.joint_acceleration = (np.array(self.joint_velocity) - np.array(self.joint_velocity_pre)) / self.SIMULATION_STEP_DELAY
+        reward,done = self.update_reward()
+        info = {}
+        return np.concatenate((self.ee_pos,self.target_position)), reward, done, False, info
+    
+    def update_reward(self):
+        reward = 0
+        done = False
+        out = [abs(self.ee_pos[i]) > abs(self.high_obs[i]) for i in range(len(self.ee_pos))]        # print(distance_2d)
+        if self.step_counter > 500:
+            # reward = -10
+            done = True 
+            print("Fail!")
+        # elif True in out:
+        #     reward = -10
+        #     done = True
+        elif np.linalg.norm(self.distance_3d) <= 0.03:
+            reward = 20
+            done = True
+            print("Reach!")
+        else:
+            # reward = np.exp(-20*np.linalg.norm(self.distance_3d)) - 0.0001 * np.linalg.norm(self.joint_acceleration)
+            # reward = -0.005 * self.distance_3d
+            reward = np.exp(-20*np.linalg.norm(self.distance_3d)) - 0.01
+        
+        return reward,done
+    
+    def reset(self,seed = None):
+        self.robot.reset()
+        info = {}
+        self.step_counter = 0
+        (self.ee_pos,self.ee_ori) = self.robot.get_current_pose()
+        # self.joint_velocity = self.robot.get_joint_obs()['velocities']
+        # target position
+        x = np.random.uniform(self.x_lower_bound, self.x_higher_bound)
+        y = np.random.uniform(self.y_lower_bound, self.y_higher_bound)
+        z = self.height
+        self.target_position = np.array([x,y,z])
+        p.removeUserDebugItem(self.line_id)
+        self.line_id = p.addUserDebugLine([x,y,z],[x,y,z+0.2],[1,0,0],4)
+        # self.distance_3d = (np.array(self.ee_pos)-self.target_position)
+
+        # print(self.distance_3d)
+        return np.concatenate((self.ee_pos,self.target_position)),info
+    
+    def close(self):
+        p.disconnect(self.physicsClient)
+
+# if __name__ == "__main__":
+#     robot = MagneticReach(pos=(0,0.5,0),ori=(0,0,0))
+#     robot.load()
